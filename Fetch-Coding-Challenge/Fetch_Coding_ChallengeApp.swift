b9629e027cba6ea3efb6b95fd932a441029c23b2
chaekweugo <chaekweugo@gmail.com>
2023-07-11 18:58:38 -0500

    Initial Commit

---
+++ Fetch-Coding-Challenge/Fetch_Coding_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Coding_ChallengeApp.swift
+//  Fetch-Coding-Challenge
+//
+//  Created by Ugonna Oparaochaekwe on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_Coding_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
